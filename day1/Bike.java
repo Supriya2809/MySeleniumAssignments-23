26e0849f906837eba15157e4c6c34cc7050c4ccb
Supriya <44491889+Supriya2809@users.noreply.github.com>
2023-03-26 13:30:15 +0800

    Add files via upload

---
+++ day1/Bike.java
@@ -0,0 +1,15 @@
+	package week1.day1;
+	
+	public class Bike {
+	
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+	Car objCar=new Car();
+			Bike objBike=new Bike();
+			objCar.applyBreak();
+			objCar.soundHorn();
+			
+	
+		}
+	
+	}
---
+++ day1/Browser.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+public class Browser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is my browser");
+		Chrome objChrome=new Chrome();
+		objChrome.PrintName();
+		objChrome.getName();
+
+	}
+
+}
